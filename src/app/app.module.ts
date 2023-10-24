8315baf0008da62190e2231c0b4bdeddfc4ebc7d
Ali Alkhatib <a.alkhatib@dg-cash.com>
2023-10-25 00:03:44 +0300

    Create the auth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthComponent } from './features/auth/auth.component';
+import { HomeComponent } from './features/home/home.component';
+import { BracketComponent } from './features/bracket/bracket.component';
+import { AboutComponent } from './features/about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    BracketComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
