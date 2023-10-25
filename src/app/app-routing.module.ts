ca5adac715b87195bedc9fd1e4b5042f70bdda74
Ali Alkhatib <a.alkhatib@dg-cash.com>
2023-10-26 00:05:31 +0300

    Home and Bracket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
+import { HomeComponent } from './features/home/home.component';
+import { BracketComponent } from './features/bracket/bracket.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'bracket', component: BracketComponent },
   { path: '',   redirectTo: '/auth', pathMatch: 'full' },
 ];
 
